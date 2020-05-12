2c39b19777b1e1a5b38e86906f0736ca4552a9f3
서연 박 <sally9476@gc.gachon.ac.kr>
2020-05-13 02:21:54 +0900

    .

--- Algorithm/1932.cpp
+++ Algorithm/1932.cpp
@@ -6,24 +6,33 @@ using namespace std;
 int main()
 {
 	int n;
+	int input[501] = { 0 };
 	int temp[501] = { 0 };
 	int sum[501] = { 0 };
-	int fValue = 0;
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i + 1; j++) {
-			cin >> temp[j];
-			
+			cin >> input[j];
 
-			sum[j] = temp[j] + max(sum[j], sum[j - 1]);
-
-
-			cout << sum[j] << ' ';
+			if(j == 0)
+				sum[j] += input[j];
+			else
+				sum[j] = input[j] + max(temp[j], temp[j - 1]);
 		}
-		cout << endl;
+
+		for (int k = 0; k < i + 1; k++) {
+			temp[k] = sum[k];
+		}
 	}
 
+	int result = sum[0];
+	for (int i = 0; i < n; i++) {
+		if (result < sum[i])
+			result = sum[i];
+	}
+
+	cout << result;
 
 	return 0;
 }
