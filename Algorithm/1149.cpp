b3a8fa58b05a849ae554d13bd0b054ba7a7aaea0
seoyeon0413 <44600155+seoyeon0413@users.noreply.github.com>
2020-05-14 22:47:04 +0900

    1149 success

--- Algorithm/1149.cpp
+++ Algorithm/1149.cpp
@@ -3,108 +3,35 @@
 
 using namespace std;
 
-typedef struct RGB {
-	int r;
-	int g;
-	int b;
-} RGB;
-
-int N;
-RGB rgb[1001];
-int selected[1001];
-int result = 0;
-
-int check(int min, int i)
-{
-	if (min == rgb[i].r)
-		return 0;
-	if (min == rgb[i].g)
-		return 1;
-	if (min == rgb[i].b)
-		return 2;
-}
-
-int minRGB(int i, int n, int start)
-{
-	
-	
-	/*
-	int min = 10000;
-
-	if (i == 0 && start == 1) {
-		selected[i] = 1;
-		return result += rgb[i].r;
-	}
-	if (i == 0 && start == 2) {
-		selected[i] = 2;
-		return result += rgb[i].g;
-	}
-	if (i == 0 && start == 3) {
-		selected[i] = 3;
-		return result += rgb[i].b;
-	}
-
-
-	if ((rgb[i].r <= rgb[i].g) && (rgb[i].r <= rgb[i].b) && selected[i - 1] != 1) {
-		min = rgb[i].r;
-		selected[i] = 1;
-	}
-	if ((rgb[i].g <= rgb[i].r) && (rgb[i].g <= rgb[i].b) && selected[i - 1] != 2) {
-		min = rgb[i].g;
-		selected[i] = 2;
-	}
-	if ((rgb[i].b <= rgb[i].r) && (rgb[i].b <= rgb[i].g) && selected[i - 1] != 3) {
-		min = rgb[i].b;
-		selected[i] = 3;
-	}
-
-	if (i == N - 1)
-		result = 0;
-
-	return result += minRGB(i++, n++, start);
-	*/
-	/*
-	for (int i = 0; i < n; i++) {
-		if (i == 0) {
-			if ((rgb[i].r < rgb[i].g) && (rgb[i].r < rgb[i].b)) {
-
-			}
-			selected[i] = check(m, i);
-		}
-		else {
-			if (selected[i - 1] == 0) {
-				m = min(rgb[i].g, rgb[i].b);
-				selected[i] = check(m, i);
-			}
-			if (selected[i - 1] == 1) {
-				m = min(rgb[i].r, rgb[i].b);
-				selected[i] = check(m, i);
-			}
-			if (selected[i - 1] == 2) {
-				m = min(rgb[i].r, rgb[i].g);
-				selected[i] = check(m, i);
-			}
-		}
-
-		// 26, 57, 13 -> 96
-
-		result += m;
-		//cout << 's' << selected[i] << endl;
-		//cout << m << endl;
-	}
-
-	return result;
-	*/
-}
+int rgb[1005][4];
+int cost[1005][4];
 
 int main()
-{	
+{
+	int N;
+	int result = 0;
+
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> rgb[i].r >> rgb[i].g >> rgb[i].b;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> cost[i][j];
+		}
+		rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + cost[i][0];	//첫번째 r, 두번째는 g,b중에 작은 값
+		rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + cost[i][1];	//첫번째 g, 두번째는 r,b중에 작은 값
+		rgb[i][2] = min(rgb[i - 1][0], rgb[i - 1][1]) + cost[i][2];	//첫번째 b, 두번째는 r,g중에 작은 값
 	}
 
-	cout << min(minRGB(0, 0, 2), min(minRGB(0, 0, 0), minRGB(0, 0, 1)));
+	result = min(min(rgb[N][0], rgb[N][1]), rgb[N][2]);
+	cout << result;
 
 	return 0;
 }
+/*
+rgb[][]는 계속 업데이트됨.
+rgb[1][] = rgb[0][] + cost[1][];
+rgb[2][] = rgb[1][] + cost[2][];
+rgb[3][] = rgb[2][] + cost[3][];
+rgb[4][] = rgb[3][] + cost[4][];
+...
+rgb[N][] = rgb[N-1][] + cost[N][];
+*/
