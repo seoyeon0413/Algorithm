9048275b6dcca5f8b525105a84a6ae63bb9bde0e
seoyeon0413 <44600155+seoyeon0413@users.noreply.github.com>
2021-01-14 19:05:13 +0900

    Create 6-11 (정렬 - 성적이 낮은 순서로 학생 출력하기).py

---
+++ CodingTest/6-11 (정렬 - 성적이 낮은 순서로 학생 출력하기).py
@@ -0,0 +1,11 @@
+n = int(input())
+
+arr = []
+for i in range(n):
+  input_data = input().split()
+  arr.append(input_data[0], int(input_data[1]))
+  
+ arr = sorted(arr, key=lambda x:x[1])
+ 
+ for student in arr:
+  print(student[0], end=' ')
